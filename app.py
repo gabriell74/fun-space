997f99114f77b7cae62d5833fc6a5b5a485603ff
gabriell74 <gabrielvivaldi07@gmail.com>
2025-03-21 12:40:35 +0700

    Menambah Luas Lingkaran

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Hitung luas segitiga sederhana
+# hitung luas segitiga sederhana
 def luas_segitiga():
     a = int(input("Masukkan alas segitiga: "))
     t = int(input("Masukkan tinggi segitiga: "))
@@ -7,11 +7,10 @@ def luas_segitiga():
     
 luas_segitiga()
 
-# Hitung luas persegi panjang
-def luas_persegi_panjang():
-    p = int(input("Masukkan panjang persegi: "))
-    l = int(input("Masukkan lebar persegi panjang: "))
-    luas = p * l
-    print("Luas persegi panjang adalah: ", luas)
-    
-luas_persegi_panjang()
+# Hitung luas lingkaran
+def luas_lingkaran():
+    r = int(input("Masukkan jari-jari: "))
+    luas = 3.14 * r**2
+    print("Luas lingkaran adalah: ", luas)
+
+luas_lingkaran()
